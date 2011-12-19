f5f26f7549c8b0e46d9e1ccd46c8396182036223
jc <jc@jc-laptop.(none)>
2011-12-19 18:21:29 +0100

    * transfert du projet de la league de duel à warsow-fr.net * création d'un modèle

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# utilisateurs
+
+class clan(models.Model):
+    name = models.CharField('nom', max_length=50)
+    email = models.CharField('email', max_length=200)
+    password = models.CharField('mot de passe', max_length=200)
+    def __unicode__(self):
+        return self.choice
+
+
+class player(models.Model):
+    name = models.CharField('nom', max_length=200)
+    email = models.CharField('email', max_length=200)
+    password = models.CharField('mot de passe', max_length=200)
+    birthday = models.DateTimeField('date de naissance')
+    clan = models.ForeignKey(clan)
+    def __unicode__(self):
+        return self.choice
