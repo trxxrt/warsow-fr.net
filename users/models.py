5a8dbbde44ffbc5a981e3c9d68364b77b32b6796
jc <jc@jc-laptop.(none)>
2011-12-19 21:23:14 +0100

    modification du model d'utilisateurs/clan

--- users/models.py
+++ users/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class clan(models.Model):
     name = models.CharField('nom', max_length=50)
     email = models.CharField('email', max_length=200)
+    is_playing_instagib = models.BooleanField('joue instagib')
+    is_playing_nw = models.BooleanField('joue normal weapon')
+    is_playing_race = models.BooleanField('joue race')
     password = models.CharField('mot de passe', max_length=200)
     def __unicode__(self):
         return self.choice
@@ -12,7 +15,12 @@ class clan(models.Model):
 
 class player(models.Model):
     name = models.CharField('nom', max_length=200)
+    first_name = models.CharField('prenom', max_length=200)
+    pseudo = models.CharField('pseudo IG', max_length=200)
     email = models.CharField('email', max_length=200)
+    is_playing_instagib = models.BooleanField('joue instagib')
+    is_playing_nw = models.BooleanField('joue normal weapon')
+    is_playing_race = models.BooleanField('joue race')
     password = models.CharField('mot de passe', max_length=200)
     birthday = models.DateTimeField('date de naissance')
     clan = models.ForeignKey(clan)
